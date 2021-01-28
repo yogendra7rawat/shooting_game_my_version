ce29b463d1649b136bb8c90f9e7b03cc1b07b0bf
Yogendra Singh Rawat <52587017+yogendra7rawat@users.noreply.github.com>
2021-01-28 21:37:51 +0530

    Update space_invader.py

--- space_invader.py
+++ space_invader.py
@@ -1,273 +1,230 @@
-import pygame as py
-import sys
+import pygame
+import os
+import random
 import time
 import math
-import random
 
-py.init();
-
-
-
-
-#display
-
-
-screen = py.display.set_mode((800,600));
-
-py.display.set_caption('Yogendra')
-
-icon = py.image.load('spaceship.png');
-
-py.display.set_icon(icon);
-
-
-
-Game_over  = py.font.Font('freesansbold.ttf',32);
-
-GO_x = 400;
-GO_y = 300;
-
-def over(x,y):
-    o = Game_over.render('GAME OVER',True,(255,100,10));
-    screen.blit(o,(x,y))
-
-#player
-score =0;
-font  = py.font.Font('freesansbold.ttf',32);
-
-
-
-
-
-text_x = 10;
-text_y = 10;
-
-def show(x,y):
-    s= font.render(f'Score: {score}',True,(255,0,0))
-    screen.blit(s,(x,y));
-
-player_img = py.image.load('lancelot.png'); 
-playerX = 470;
-playerY = 350;
-playerX_change =0;
-playerY_change =0;
-def player(x,y):
-    screen.blit(player_img,(x,y));
-
-
-
-    
-# enemy
-enemy_img = [];
-enemyX = []
-enemyY = []
-enemyX_change =[]
-enemyY_change =[]
-global num_of_enemy
-num_of_enemy = 13;
-
-for i in range(num_of_enemy):
-
-    enemy_img.append(py.image.load('target.png'));
-    enemyX.append(random.randint(0,800));
-    enemyY.append(random.randint(50,150));
-    enemyX_change.append(0.2);
-    enemyY_change.append(40);
-
-
-def enemy(x,y,i):
-    screen.blit(enemy_img[i],(x,y));
-
-
-
-
-
-#meteorite
-
-m_img = [];
-mX = []
-mY = []
-mX_change =[]
-mY_change =[]
-global num_of_m
-num_of_m = 3;
-
-for i in range(num_of_enemy):
-
-    m_img.append(py.image.load('meteorite.png'));
-    mX.append(random.randint(0,800));
-    mY.append(random.randint(50,150));
-    mX_change.append(0.5);
-    mY_change.append(40);
-
-
-
-
-# bullet
-
-
-bulletX =0;
-bulletY = playerY;
-bullet_state ='ready';
-bulletY_change =1.2;
-bullet_img = py.image.load('bullet.png');
-
-
-    
-
-def fire(current_x,current_y):
-    global bullet_state;
-    bullet_state = 'fire';
-    screen.blit(bullet_img,(current_x+16,current_y+10));
-    
-
-
-
-
-def hit(enemyX,enemyY,bulletX,bulletY):
-    distance = math.sqrt(math.pow(enemyX-bulletX,2)+math.pow(enemyY-bulletY,2));
-
-    if(distance<27):
-        return True;
-    else:
-        return False;
-
-
-
-
-background  = py.image.load('space.png');
-
-running = True
-  
-while running:
-    
-    screen.fill((0,0,0));
-
-    screen.blit(background,(0,0));
-    
-
-    
-    show(text_x,text_y);
-    for event in py.event.get():
-        if event.type == py.QUIT:
-            running = False;
-
-        if(event.type == py.KEYDOWN):
-            if(event.key == py.K_LEFT):
-                playerX_change -=0.3;
-                
-                
-            if(event.key == py.K_RIGHT):
-                playerX_change+=0.3;
-                
-
-            if(event.key == py.K_UP):
-                playerY_change-=0.3;
-
-            if(event.key == py.K_DOWN):
-                playerY_change+=0.3;
-
-            if event.key == py.K_SPACE and bullet_state == 'ready':
-                bulletX = playerX
-                fire(bulletX,bulletY);
-                
-                
-
-        if event.type == py.KEYUP:
-            if event.key == py.K_LEFT or event.key == py.K_RIGHT:
-                playerX_change = 0;
-               
-            if event.key == py.K_UP or event.key == py.K_DOWN:
-                 playerY_change = 0;
-
-            
-    
-                 
-
-                 
-
-  
-
-    
-    playerX+=playerX_change;
-    playerY+=playerY_change;
-    
-    if playerX<=0:
-        playerX = 0;
-
-    elif playerX>=736:
-        playerX = 736;
-
-
-    if playerY<=0:
-        playerY = 0;
-
-    elif playerY>=536:
-        playerY = 536;
-    
-        
-    
-
-    # enemy part
-    for i in range(num_of_enemy):
-        
-        enemyX[i]+=enemyX_change[i];
-    
-    
-        if(enemyX[i]<=0):
-            enemyX_change[i] = 0.2;
-            enemyY[i]+=enemyY_change[i];
-
-        elif enemyX[i]>=736:
-            enemyX_change[i] = -0.2;
-            enemyY[i]+=enemyY_change[i];
-
-
-        collosion = hit(enemyX[i],enemyY[i],bulletX,bulletY);
-
-        if(collosion):
-            bulletY = playerY;
-            bullet_state = 'ready';
-            score+=1;
-            print("score = ",score);
-            enemyX[i] = random.randint(0,800);
-            enemyY[i] = random.randint(50,150);
-            num_of_enemy-=1;
-
-        enemy(enemyX[i],enemyY[i],i);
-
-
-    if(num_of_enemy == 0):
-        over(GO_y,GO_y);
-
-
-    #for i in range(num_of_m):
-
-    #bullet part
-
-
-    if bulletY<=0:
-        bulletY = playerY;
-        bullet_state = 'ready';
-
-    if bullet_state is 'fire':
-        fire(bulletX,bulletY);
-        bulletY-=bulletY_change;
-
-
-
-
-    
-
-    
-    player(playerX,playerY); # player display
-    
-    
-
-
-
-
-    py.display.update();
-
-
-py.quit()
+pygame.init()
+WIDTH = 700
+HEIGHT = 700
+FPS = 60
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+
+blue_ship = pygame.image.load(os.path.join("assets","pixel_ship_blue_small.png"))
+red_ship = pygame.image.load(os.path.join("assets","pixel_ship_red_small.png"))
+yellow_ship = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
+green_ship = pygame.image.load(os.path.join("assets","pixel_ship_green_small.png"))
+
+
+
+blue_laser = pygame.image.load(os.path.join("assets","pixel_laser_blue.png"))
+green_laser = pygame.image.load(os.path.join("assets","pixel_laser_green.png"))
+red_laser =  pygame.image.load(os.path.join("assets","pixel_laser_red.png"))
+yellow_laser =  pygame.image.load(os.path.join("assets","pixel_laser_yellow.png"))
+
+
+background = pygame.transform.scale(pygame.image.load(os.path.join("assets","background-black.png")),(WIDTH,HEIGHT))
+
+
+
+
+bullets = []
+Enemies = []
+
+class Player():
+	def __init__(self,x,y,heal):
+		self.x = x
+		self.y = y
+		self.ship_img = yellow_ship
+		self.laser_img = yellow_laser
+		self.heal = heal
+		self.mask = pygame.mask.from_surface(self.ship_img)
+		
+		
+		
+	def draw(self,screen):
+		screen.blit(self.ship_img,(self.x,self.y))
+		self.health(screen)
+		
+	def health(self,screen):
+		pygame.draw.rect(screen,(255,0,0),(self.x,self.y+self.ship_img.get_height()//2+50,self.ship_img.get_width(),10))
+		pygame.draw.rect(screen,(0,210,0),(self.x,self.y+self.ship_img.get_height()//2+50,self.ship_img.get_width()-5*(100-self.heal),10))
+		
+		
+	def bullet(self,screen):
+		screen.blit(self.laser_img,(self.x,self.y-self.ship_img.get_height()//2))
+	
+
+	
+class Enemy():
+	COLOR_MAP = {
+                "red": (red_ship, red_laser),
+                "green": (green_ship,green_laser),
+                "blue": (blue_ship, blue_laser)
+                }
+	def __init__(self,x,y,color,vel):
+		self.x = x
+		self.y = y
+		self.vel = vel
+		self.color = color
+		self.ship_img, self.laser_img = self.COLOR_MAP[color]
+		self.mask = pygame.mask.from_surface(self.ship_img)
+		
+	def draw(self,screen):
+		screen.blit(self.ship_img,(self.x,self.y))
+		
+		
+	def move(self):
+		self.y +=3
+		
+	def shoot(self):
+		self.vel +=0.1
+		screen.blit(self.laser_img,(self.x-15,self.y+self.vel))
+			
+	
+
+
+def collide(obj1, obj2):
+	offset_x = obj2.x-obj1.x
+	offset_y = obj2.y-obj1.y
+	return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
+	
+main_font = pygame.font.Font("freesansbold.ttf", 32)
+lost_font = pygame.font.Font("freesansbold.ttf", 50)
+
+
+
+
+def draw_win(x,y,lives,heal):
+	screen.fill((0,0,0))
+	
+	screen.blit(background,(0,0))
+	#Space_ship
+	space_ship = Player(x,y,heal)
+	space_ship.draw(screen)
+	# health 
+	# ~ Health = Laser(x,y,yellow_ship.get_height(),heal)
+	# ~ Health.draw(screen)
+	
+	for enemy in Enemies:
+		enemy.draw(screen)
+		if random.randrange(0, 2*60) == 1:
+			enemy.shoot()
+		
+	#laser 
+	for bullet in bullets:
+		bullet.bullet(screen)
+		
+	
+	if(lives<0):
+		lost_label = lost_font.render("You Lost!!", 1, (255,255,255))
+		screen.blit(lost_label, (WIDTH/2 - lost_label.get_width()/2, 350))
+	
+	lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+	screen.blit(lives_label,(10,10))
+	pygame.display.update()
+
+def main(screen):
+	x =200
+	y=350
+	lives=5
+	length =5
+	run  = True
+	total_bullet =0
+	heal = 100
+	space_ship = Player(x,y,heal)
+	enemy = Enemy(random.randrange(50, WIDTH-100),random.randrange(-1500, -100),random.choice(["red","blue","green"]),-10)
+	clock = pygame.time.Clock()
+	flag=0
+	while run:
+		clock.tick(FPS)
+		for enemy in Enemies:
+			if enemy.y>HEIGHT:
+				lives-=1
+				heal-=1
+				Enemies.pop(Enemies.index(enemy))
+		
+		if total_bullet>0:
+			total_bullet +=1
+		if total_bullet>5:
+			total_bullet =0
+		for bullet in bullets:
+			if collide(enemy,bullet):
+				bullets.remove(bullet)
+				Enemies.remove(enemy)
+			if bullet.y<HEIGHT and bullet.y>0:
+				bullet.y-=4
+			else:
+				bullets.remove(bullet)
+				
+				
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run  = False
+			
+		keys = pygame.key.get_pressed()
+		
+		if keys[pygame.K_a]:
+			x -=2
+		if keys[pygame.K_d]:
+			x+=2
+		if keys[pygame.K_w]:
+			y-=2
+		if keys[pygame.K_s]:
+			y+=2
+		
+		if keys[pygame.K_SPACE] and total_bullet==0:
+			print(x,y)
+			if(len(bullets)<5):
+				Bullet = Player(x,y,heal)
+				bullets.append(Bullet)
+			
+			total_bullet = 1
+			
+		
+		if len(Enemies)==0:
+			length+=5
+			for i in range(length):
+				enemy_x = random.randrange(50, WIDTH-100)
+				enemy_y  = random.randrange(-1500, -100)
+				enemy = Enemy(enemy_x, enemy_y,random.choice(["red","blue","green"]),-10)
+				Enemies.append(enemy)
+				
+		
+
+					
+		draw_win(x,y,lives,heal)
+		
+		for enemy in Enemies[:]:
+			enemy.move()
+			enemy.shoot()
+							
+			if(collide(enemy,space_ship)):
+				print("HIT")
+				heal-=1
+				Enemies.pop(Enemies.index(enemy))
+
+		
+		
+		
+		if(x+yellow_ship.get_width()<WIDTH and y+yellow_ship.get_height()<HEIGHT and (x>0 and y>0)):
+			continue
+			
+		if(x+yellow_ship.get_width()>= WIDTH):
+			x = WIDTH - yellow_ship.get_width()
+		if(x<=0):
+			x=0			
+		if(y+yellow_ship.get_height() >= HEIGHT):
+			y = WIDTH - yellow_ship.get_height()
+		
+		if(y<=0):
+			y=0
+		
+		
+		
+		
+			
+	pygame.quit()
+	
+	
+main(screen)
